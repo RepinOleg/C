a9f0687858c746452a1632714e23974f44fe909a
weezingq <rolegoci@yandex.ru>
2023-08-28 23:45:57 +0300

    delete

--- decimal_lib/src/common/s21_getters_decimal.c
+++
@@ -1,6 +0,0 @@
-#include "../s21_decimal.h"
-
-int s21_get_sign(s21_decimal value) { return (value.bits[3] & (1 << 31)) != 0; }
-int s21_get_exp(s21_decimal value) {
-  return (value.bits[3] >> 16) & ((1 << 8) - 1);
-}
--- string_library_on_C/src/s21_memset.c
+++
@@ -1,9 +0,0 @@
-#include "s21_string.h"
-
-void *s21_memset(void *dest, int c, s21_size_t n) {
-  unsigned char *s = dest;
-  for (s21_size_t i = 0; i < n; i++) {
-    s[i] = c;
-  }
-  return (s);
-}
